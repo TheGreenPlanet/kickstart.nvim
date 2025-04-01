21c26f7626972ba8e3ac57efb0703f653f0d9e2f
TheGreenPlanet <48806927>
2025-04-01 13:33:37 +0200

    Add better-escape plugin

---
+++ lua/custom/plugins/better-escape.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    'max397574/better-escape.nvim',
+    config = function()
+      require('better_escape').setup {
+        timeout = 150,
+        default_mappings = true,
+        mappings = {
+          i = {
+            j = {
+              k = '<Esc>',
+            },
+          },
+        },
+      }
+    end,
+  },
+}
