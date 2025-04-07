b26d45daf59cb8bb9fd0cc8a27243c435d46417c
TheGreenPlanet <48806927>
2025-04-07 16:31:52 +0200

    Increase jk window time

--- lua/custom/plugins/better-escape.lua
+++ lua/custom/plugins/better-escape.lua
@@ -3,7 +3,7 @@ return {
     'max397574/better-escape.nvim',
     config = function()
       require('better_escape').setup {
-        timeout = 150,
+        timeout = 200,
         default_mappings = true,
         mappings = {
           i = {
