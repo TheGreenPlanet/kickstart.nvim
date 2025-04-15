4aaeca707367744f7c97acc0f2df1fc00829e190
TheGreenPlanet <48806927+TheGreenPlanet@users.noreply.github.com>
2025-04-15 22:42:02 +0200

    Increase jk window

--- lua/custom/plugins/better-escape.lua
+++ lua/custom/plugins/better-escape.lua
@@ -3,7 +3,7 @@ return {
     'max397574/better-escape.nvim',
     config = function()
       require('better_escape').setup {
-        timeout = 200,
+        timeout = 250,
         default_mappings = true,
         mappings = {
           i = {
